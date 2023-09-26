75d3f888cd7ebad5cbcfa570a19c792eaef4be87
Avram Đorđević <48069158+avramdj@users.noreply.github.com>
2023-09-26 21:11:55 +0200

    Update __main__.py

--- scraper/__main__.py
+++ scraper/__main__.py
@@ -18,7 +18,7 @@ def get_local_time():
 
 
 def main():
-    CFG_FILE = os.environ.get("CFG_FILE", "config.json")
+    CFG_FILE = os.environ.get("CFG_FILE", "/app/config.json")
     # in seconds
     SLEEP_TIME = float(os.environ.get("SLEEP_TIME", 30))
 
